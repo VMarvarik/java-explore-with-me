d1bdb8effae0d85d660566dbf7cb4140fd1bebc8
VarvaraMatveeva <forestknight567@outlook.com>
2023-07-28 20:58:56 +0300

    Feat: repo and model fixes

--- ewm-stats-service/stat-dto/src/main/java/ru/practicum/statdto/ViewStatsDto.java
+++ ewm-stats-service/stat-dto/src/main/java/ru/practicum/statdto/ViewStatsDto.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class ViewStatsDto {
+    @NotBlank
     private String app;
+    @NotBlank
     private String uri;
+    @NotBlank
     private Long hits;
 }
