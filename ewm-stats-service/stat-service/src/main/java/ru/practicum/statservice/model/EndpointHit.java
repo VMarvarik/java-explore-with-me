efb250468e2f39a52236653d52da28a5f3690a9c
VarvaraMatveeva <forestknight567@outlook.com>
2023-07-28 20:43:37 +0300

    Feat: models fixes

--- ewm-stats-service/stat-service/src/main/java/ru/practicum/statservice/model/EndpointHit.java
+++ ewm-stats-service/stat-service/src/main/java/ru/practicum/statservice/model/EndpointHit.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
@@ -19,12 +18,8 @@ public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
     private String app;
-    @NotBlank
     private String uri;
-    @NotBlank
     private String ip;
-    @NotBlank
     private LocalDateTime created;
 }
