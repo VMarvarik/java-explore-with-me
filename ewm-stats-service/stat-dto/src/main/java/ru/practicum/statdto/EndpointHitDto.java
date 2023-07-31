dce934cd516abf54f6a505d48fa322f1b2c6f386
VarvaraMatveeva <forestknight567@outlook.com>
2023-07-31 19:50:57 +0300

    Feat: mapper fixes

--- ewm-stats-service/stat-dto/src/main/java/ru/practicum/statdto/EndpointHitDto.java
+++ ewm-stats-service/stat-dto/src/main/java/ru/practicum/statdto/EndpointHitDto.java
@@ -1,11 +1,13 @@
 package ru.practicum.statdto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,5 +21,6 @@ public class EndpointHitDto {
     @NotBlank(message = "ip не может быть пустым или null")
     private String ip;
     @NotBlank(message = "timestamp не может быть пустым или null")
-    private String timestamp;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime timestamp;
 }
