ab49d35b388f9d09147f067c0e9f8091ab86497b
VarvaraMatveeva <forestknight567@outlook.com>
2023-08-11 22:49:55 +0300

    feat: main svc

--- statistics/stat-service/src/main/java/ru/practicum/statservice/repository/StatRepository.java
+++ statistics/stat-service/src/main/java/ru/practicum/statservice/repository/StatRepository.java
@@ -1,4 +1,4 @@
-package ru.practicum.statservice;
+package ru.practicum.statservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
