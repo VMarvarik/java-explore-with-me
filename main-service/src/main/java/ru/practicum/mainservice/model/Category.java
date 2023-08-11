66511b8343f483e0fe72eb6590762730b5da4286
VarvaraMatveeva <forestknight567@outlook.com>
2023-08-12 01:33:23 +0300

    feat: main svc

---
+++ main-service/src/main/java/ru/practicum/mainservice/model/Category.java
@@ -0,0 +1,26 @@
+package ru.practicum.mainservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "category", schema = "public")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+}
---
+++ statistics/stat-client/src/main/java/ru/practicum/statclient/StatClient.java
@@ -0,0 +1,53 @@
+package ru.practicum.statclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.practicum.statdto.EndpointHitDto;
+import ru.practicum.statdto.ViewStatsDto;
+
+import java.util.List;
+
+@Slf4j
+public class StatClient {
+    private static final String STAT_SERVER_URL = System.getenv().get("STATS_SERVER_URL");
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl(STAT_SERVER_URL)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+
+    public void addHit(EndpointHitDto endpointHitDto) {
+        log.trace("Sending endpointHitDto={} to stats-server.", endpointHitDto);
+        webClient
+                .post()
+                .uri("/hit")
+                .bodyValue(endpointHitDto)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+        log.trace("EndpointHitDto sent to stats-server.");
+    }
+
+    public List<ViewStatsDto> getStats(String start, String end, List<String> uris, boolean unique) {
+        StringBuilder urisToSend = new StringBuilder();
+        for (String uri : uris) {
+            urisToSend.append(uri).append(",");
+        }
+        log.trace("Requesting Stats from stats-server with params: start={}, end={}, uris={}, unique={}.", start, end, uris, unique);
+        return webClient
+                .get()
+                .uri(builder -> builder
+                        .path("/stats")
+                        .queryParam("start", start)
+                        .queryParam("end", end)
+                        .queryParam("uris", urisToSend)
+                        .queryParam("unique", unique)
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<ViewStatsDto>>() {
+                })
+                .block();
+    }
+}
