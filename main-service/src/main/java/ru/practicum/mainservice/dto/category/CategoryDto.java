ee336054016911a00a28fbe12de8e84f53a0c6ff
VarvaraMatveeva <forestknight567@outlook.com>
2023-08-12 18:46:08 +0300

    fix: dto fix

--- main-service/src/main/java/ru/practicum/mainservice/dto/category/CategoryDto.java
+++ main-service/src/main/java/ru/practicum/mainservice/dto/category/CategoryDto.java
@@ -2,6 +2,8 @@ package ru.practicum.mainservice.dto.category;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @Data
 @ToString
 @Builder
@@ -10,5 +12,6 @@ import lombok.*;
 public class CategoryDto {
     private Long id;
 
+    @Size(min = 1, max = 50, message = "Имя не может быть длиной более 50 символов")
     private String name;
 }
